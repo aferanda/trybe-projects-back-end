c4d943ac005fc3a284ab9538ec43d66bdca226de
Amanda Fernandes <aferanda@gmail.com>
2022-02-04 10:58:10 -0300

    Desafio 7

---
+++ desafio7.sql
@@ -0,0 +1,15 @@
+SELECT
+  ART.artista AS artista,
+  ALB.album AS album,
+  COUNT(S.artista_id) AS seguidores
+FROM
+  SpotifyClone.artista AS ART
+  INNER JOIN SpotifyClone.album AS ALB ON ALB.artista_id = ART.artista_id
+  INNER JOIN SpotifyClone.seguidor AS S ON S.artista_id = ART.artista_id
+GROUP BY
+  artista,
+  album
+ORDER BY
+  seguidores DESC,
+  artista ASC,
+  album ASC;
