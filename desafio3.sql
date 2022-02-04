ec963777a9e5920b29c909571f73c189ed6a606d
Amanda Fernandes <aferanda@gmail.com>
2022-02-03 21:40:42 -0300

    Formata código

--- desafio3.sql
+++ desafio3.sql
@@ -1,11 +1,12 @@
-SELECT 
-  U.usuario AS usuario, 
+SELECT
+  U.usuario AS usuario,
   COUNT(HR.usuario_id) AS qtde_musicas_ouvidas,
   ROUND(SUM(C.duracao_segundos / 60), 2) AS total_minutos
-FROM SpotifyClone.usuario AS U
-INNER JOIN SpotifyClone.historico_reproducao AS HR
-ON HR.usuario_id = U.usuario_id
-INNER JOIN SpotifyClone.cancao AS C
-ON C.cancao_id = HR.cancao_id
-GROUP BY HR.usuario_id
-ORDER BY usuario ASC;
+FROM
+  SpotifyClone.usuario AS U
+  INNER JOIN SpotifyClone.historico_reproducao AS HR ON HR.usuario_id = U.usuario_id
+  INNER JOIN SpotifyClone.cancao AS C ON C.cancao_id = HR.cancao_id
+GROUP BY
+  HR.usuario_id
+ORDER BY
+  usuario ASC;
