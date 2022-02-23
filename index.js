153cc73996bcd83ed29a4ce6e49df4381b12d122
Amanda Fernandes <aferanda@gmail.com>
2022-02-23 18:55:08 -0300

    atualiza index

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+const express = require('express');
 require('dotenv').config();
+
+const errorMiddleware = require('./middlewares/error.middleware');
+const { productsRouter } = require('./routes/index.routes');
+
+const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/products', productsRouter);
+// app.use('/sales', salesRouter);
+
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
