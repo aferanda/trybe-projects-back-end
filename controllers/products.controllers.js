9b21dc85825bcacab9d60959b1898836832519b0
Amanda Fernandes <aferanda@gmail.com>
2022-02-24 00:18:59 -0300

    refatora retorno das funções

--- controllers/products.controllers.js
+++ controllers/products.controllers.js
@@ -1,14 +1,14 @@
 const productsServices = require('../services/products.services');
 
 const getAll = async (_req, res) => {
-  const products = await productsServices.getAll();
+  const [products] = await productsServices.getAll();
   return res.status(200).json(products);
 };
 
 const getById = async (req, res) => {
   try {
     const { id } = req.params;
-    const product = await productsServices.getById(id);
+    const [product] = await productsServices.getById(id);
 
     if (!product) return res.status(404).json({ message: 'Product not found' });
     return res.status(200).json(product);
--- controllers/sales.controllers.js
+++ controllers/sales.controllers.js
@@ -1,14 +1,14 @@
 const salesServices = require('../services/sales.services');
 
 const getAll = async (_req, res) => {
-  const sales = await salesServices.getAll();
+  const [sales] = await salesServices.getAll();
   return res.status(200).json(sales);
 };
 
 const getById = async (req, res) => {
   try {
     const { id } = req.params;
-    const sales = await salesServices.getById(id);
+    const [sales] = await salesServices.getById(id);
 
     if (sales.length === 0) {
     return res.status(404).json({ message: 'Sale not found' });
