d0bac5d42cae05a8d7744bd0c8f6d66c75b01761
Amanda Fernandes <aferanda@gmail.com>
2022-02-25 00:50:09 -0300

    atualiza rotas

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const productsControllers = require('../controllers/products.controllers');
+const productsMiddleware = require('../middlewares/products.middleware');
 
 const router = express.Router();
 
 router.get('/', productsControllers.getAll);
 
 router.get('/:id', productsControllers.getById);
+
+router.post('/', productsMiddleware, productsControllers.create);
+
+router.put('/', productsMiddleware, () => {});
 
 module.exports = router;
--- routes/sales.routes.js
+++ routes/sales.routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const salesControllers = require('../controllers/sales.controllers');
+const salesMiddleware = require('../middlewares/sales.middleware');
 
 const router = express.Router();
 
 router.get('/', salesControllers.getAll);
 
 router.get('/:id', salesControllers.getById);
+
+router.post('/', salesMiddleware, () => {});
+
+router.put('/', salesMiddleware, () => {});
 
 module.exports = router;
