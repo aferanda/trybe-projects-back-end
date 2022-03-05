c1beacfe3c01f3aecf874a9020fcd38159698cd8
Amanda Fernandes <aferanda@gmail.com>
2022-03-05 10:39:01 -0300

    refatora testes dos models

--- test/unit/models/salesModels.js
+++ test/unit/models/salesModels.js
@@ -5,132 +5,85 @@ const DB = require("../../../models/connection.models");
 const salesModels = require("../../../models/sales.models");
 
 describe("SALES MODELS TESTS", () => {
+  before(() => {
+    const modelsResponse = [[
+      {
+        saleId: 1,
+        date: "2022-02-24T17:06:40.000Z",
+        productId: 1,
+        quantity: 5,
+      },
+      {
+        saleId: 1,
+        date: "2022-02-24T17:06:40.000Z",
+        productId: 2,
+        quantity: 10,
+      },
+      {
+        saleId: 2,
+        date: "2022-02-24T17:06:40.000Z",
+        productId: 3,
+        quantity: 15,
+      },
+    ]];
+    sinon.stub(DB, "execute").resolves(modelsResponse);
+  });
+
+  after(() => {
+    DB.execute.restore();
+  });
+
   describe("Busca vendas no DB", () => {
     describe("1 - Busca todos as vendas no DB", () => {
-      before(async () => {
-        const execute = [[]];
-        sinon.stub(DB, "execute").resolves(execute);
-      });
-
-      after(async () => {
-        DB.execute.restore();
-      });
-
-      describe("1.1 - quando não existe vendas no DB", () => {
-        it("retorna um array vazio", async () => {
-          const response = await salesModels.getAll();
-          expect(response).to.be.deep.equal([]);
-        });
-      });
-
-      describe("1.2 - quando existe vendas no DB", () => {
-        const salesData = [
-          {
-            saleId: 1,
-            date: "2022-02-24T15:17:45.000Z",
-            productId: 1,
-            quantity: 5,
-          },
-          {
-            saleId: 1,
-            date: "2022-02-24T15:17:45.000Z",
-            productId: 2,
-            quantity: 10,
-          },
-          {
-            saleId: 2,
-            date: "2022-02-24T15:17:45.000Z",
-            productId: 3,
-            quantity: 15,
-          },
-        ];
-
-        before(() => {
-          sinon.stub(salesModels, "getAll").resolves(salesData);
-        });
-
-        after(() => {
-          salesModels.getAll.restore();
-        });
-
-        it("retorna todos os vendas do DB", async () => {
-          const response = await salesModels.getAll();
-          expect(response).to.be.deep.equal(salesData);
-        });
-
+      describe("1.1 - quando existe vendas no DB", () => {
         it("retorna um array", async () => {
           const response = await salesModels.getAll();
           expect(response).to.be.an("array");
         });
-
-        it("o array não está vazio", async () => {
+        it("esse array tem 3 posições", async () => {
           const response = await salesModels.getAll();
-          expect(response).to.be.not.empty;
-        });
-
-        it("tal array possui tamanho igual a 3", async () => {
-          const item = await salesModels.getAll();
-          expect(item).to.have.length(3);
+          expect(response).to.have.length(3);
         });
       });
     });
 
     describe("2 - Busca apenas uma venda no DB por seu ID", () => {
-      before(async () => {
-        const execute = [[]];
-        sinon.stub(DB, "execute").resolves(execute);
-      });
-
-      after(async () => {
-        DB.execute.restore();
-      });
-
-      describe("2.1 - quando não existe uma venda com o ID informado", () => {
-        it("retorna um array vazio", async () => {
-          const response = await salesModels.getById();
-          expect(response).to.be.empty;
-        });
-      });
-
-      describe("2.2 - quando existe vendas com o ID informado", () => {
-        before(() => {
-          sinon.stub(salesModels, "getById").resolves([
-            {
-              date: "2022-02-24T15:09:58.000Z",
-              productId: 1,
-              quantity: 5,
-            },
-            {
-              date: "2022-02-24T15:09:58.000Z",
-              productId: 2,
-              quantity: 10,
-            },
-          ]);
-        });
-
-        after(() => {
-          salesModels.getById.restore();
-        });
-
+      describe("2.1 - quando existe uma venda com o ID informado", () => {
         it("retorna um array", async () => {
           const response = await salesModels.getById(1);
           expect(response).to.be.an("array");
         });
+      });
+    });
+  });
 
-        it("o array não está vazio", async () => {
-          const response = await salesModels.getById(1);
-          expect(response).to.be.not.empty;
+  describe("Atualiza vendas no DB", () => {
+    describe("1 - Cria uma venda no DB", () => {
+      describe("1.1 - quando cria uma venda com o produto e quantidades informados", () => {
+        it("retorna undefined", async () => {
+          const response = await salesModels.createSaleId();
+          expect(response).to.be.a("undefined");
         });
 
-        it('tal array possui tamanho igual a 2', async () => {
-          const item = await salesModels.getById(1);
-          expect(item).to.have.length(2);
+        it("retorna um array", async () => {
+          const response = await salesModels.createSalesProducts(4, 1, 10);
+          expect(response).to.be.an("array");
         });
-
-        it('tal array possui 2 objetos com as propriedades: "date", "productId" e "quantity"', async () => {
-          const item = await salesModels.getById(1);
-          expect(item[0]).to.include.all.keys("date", "productId", "quantity");
-          expect(item[1]).to.include.all.keys("date", "productId", "quantity");
+      });
+    });
+    describe("2 - Atualiza uma venda no DB", () => {
+      describe("2.1 - quando atualiza uma venda", () => {
+        it("retorna um array", async () => {
+          const response = await salesModels.update(1, 10, 1);
+          expect(response).to.be.an("array");
+        });
+      });
+    });
+    describe("3 - Deleta uma venda do DB", () => {
+      describe("3.1 - quando deleta uma venda com o id informado", () => {
+        it("retorna um array", async () => {
+          const response = await salesModels.remove(1);
+          expect(response).to.be.an("array");
         });
       });
     });
