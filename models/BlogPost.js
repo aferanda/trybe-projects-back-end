32c07bb1e9d31a16bfdb24554fe1771c552abbc0
Amanda Fernandes <aferanda@gmail.com>
2022-03-11 18:08:42 -0300

    created model BlogPost

---
+++ models/BlogPost.js
@@ -0,0 +1,39 @@
+const { DataTypes } = require('sequelize');
+
+const Attributes = {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    allowNull: false,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+};
+
+module.exports = (sequelize) => {
+  const BlogPost = sequelize.define(
+    'BlogPost',
+    Attributes,
+    {
+      tableName: 'BlogPosts',
+    },
+  );
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User,
+    { foreingKey: 'user_id', as: 'user' });
+  };
+
+  return BlogPost;
+};
