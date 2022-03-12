64550189c10c824df55cb94d6bc40a0c054276bf
Amanda Fernandes <aferanda@gmail.com>
2022-03-12 19:23:41 -0300

    rename route files

---
+++ src/routes/index.js
@@ -0,0 +1,11 @@
+const loginRouter = require('./login.routes');
+const userRouter = require('./user.routes');
+const postRouter = require('./post.routes');
+const categoryRouter = require('./category.routes');
+
+module.exports = {
+  loginRouter,
+  userRouter,
+  postRouter,
+  categoryRouter,
+};
