3461e8287c0b145c284174172a99f14d4412c6be
Amanda Fernandes <aferanda@gmail.com>
2022-03-11 23:18:13 -0300

    fix: config file

--- config/config.js
+++ config/config.js
@@ -7,9 +7,6 @@ module.exports = {
     database: 'blogs_api',
     host: process.env.HOSTNAME,
     dialect: 'mysql',
-    define: {
-      underscored: true,
-    },
   },
   test: {
     username: process.env.MYSQL_USER,
