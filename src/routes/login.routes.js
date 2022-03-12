6c3254be88f4d3bdf92836dc17711e08af246bad
Amanda Fernandes <aferanda@gmail.com>
2022-03-12 20:56:29 -0300

    feat: define login route

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const { login } = require('../controllers/login.controllers');
+const loginMiddleware = require('../middlewares/login.middleware');
 
 const router = express.Router();
 
-router.post('/');
+router.post('/', loginMiddleware, login);
 
 module.exports = router;
