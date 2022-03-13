bb9ad8ed84f38ae781d248b36548b7fcee5158a4
Amanda Fernandes <aferanda@gmail.com>
2022-03-13 10:21:59 -0300

    feat: create service get user by id

--- src/services/user.services.js
+++ src/services/user.services.js
@@ -21,7 +21,17 @@ const getAllUsers = async () => {
   return { code: 200, users };
 };
 
+const getUserById = async (id) => {
+  const user = await User.findByPk(id,
+    { attributes: { exclude: ['password'] } });
+
+  if (!user) return { code: 404, message: 'User does not exist' };
+
+  return { code: 200, user };
+};
+
 module.exports = {
   createUser,
   getAllUsers,
+  getUserById,
 };
