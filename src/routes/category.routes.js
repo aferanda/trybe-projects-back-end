60a2fa44f78d0dc994a857afaf843a3ebe1f8c1d
Amanda Fernandes <aferanda@gmail.com>
2022-03-13 11:22:07 -0300

    feat: update route get all categories

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { createCategory } = require('../controllers/category.controllers');
+const { createCategory, getAllCategories } = require('../controllers/category.controllers');
 const categoryMiddleware = require('../middlewares/category.middleware');
 const authMiddleware = require('../middlewares/auth.middleware');
 
 const router = express.Router();
 
 router.post('/', authMiddleware, categoryMiddleware, createCategory);
-router.get('/');
+router.get('/', authMiddleware, getAllCategories);
 
 module.exports = router;
