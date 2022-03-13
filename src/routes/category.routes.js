0c8501761d094fbd6663ed1fc3d238518107842f
Amanda Fernandes <aferanda@gmail.com>
2022-03-13 11:14:27 -0300

    feat: update route create category

--- src/routes/category.routes.js
+++ src/routes/category.routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const { createCategory } = require('../controllers/category.controllers');
+const categoryMiddleware = require('../middlewares/category.middleware');
+const authMiddleware = require('../middlewares/auth.middleware');
 
 const router = express.Router();
 
-router.post('/');
+router.post('/', authMiddleware, categoryMiddleware, createCategory);
 router.get('/');
 
 module.exports = router;
