01793e5269ad4d4066f78b43da4748ab9973b7f5
Amanda Fernandes <aferanda@gmail.com>
2022-03-14 00:19:34 -0300

    refactor: rename const

--- src/controllers/category.controllers.js
+++ src/controllers/category.controllers.js
@@ -1,15 +1,15 @@
-const categoryService = require('../services/category.services');
+const categoryServices = require('../services/category.services');
 
 const createCategory = async (req, res) => {
   const { name } = req.body;
 
-  const { code, newCategory } = await categoryService.createCategory(name);
+  const { code, newCategory } = await categoryServices.createCategory(name);
 
   return res.status(code).json(newCategory);
 };
 
 const getAllCategories = async (req, res) => {
-  const { code, categories } = await categoryService.getAllCategories();
+  const { code, categories } = await categoryServices.getAllCategories();
 
   return res.status(code).json(categories);
 };
