b4b8d6755330f64bae46365f360a0895a920c58e
Amanda Fernandes <aferanda@gmail.com>
2022-03-14 10:56:51 -0300

    feat: create controller get post by id

--- src/controllers/post.controllers.js
+++ src/controllers/post.controllers.js
@@ -15,7 +15,17 @@ const getAllPosts = async (req, res) => {
   return res.status(code).json(posts);
 };
 
+const getPostById = async (req, res) => {
+  const { id } = req.params;
+  const { code, post, message } = await postServices.getPostById(id);
+
+  if (message) return res.status(code).json({ message });
+
+  return res.status(code).json(post);
+};
+
 module.exports = {
   createPost,
   getAllPosts,
+  getPostById,
 };
