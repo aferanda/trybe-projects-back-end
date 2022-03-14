899e11e0ae36eb7a343897f3ad9702ad6964e041
Amanda Fernandes <aferanda@gmail.com>
2022-03-14 10:57:19 -0300

    feat: update route get post by id

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const postMiddleware = require('../middlewares/post.middleware');
 const authMiddleware = require('../middlewares/auth.middleware');
-const { createPost, getAllPosts } = require('../controllers/post.controllers');
+const { createPost, getAllPosts, getPostById } = require('../controllers/post.controllers');
 
 const router = express.Router();
 
 router.post('/', authMiddleware, postMiddleware, createPost);
 router.post('/:id');
 router.get('/', authMiddleware, getAllPosts);
-router.get('/:id');
+router.get('/:id', authMiddleware, getPostById);
 
 module.exports = router;
