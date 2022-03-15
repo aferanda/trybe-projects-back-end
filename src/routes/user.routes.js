d16c369a98fc7d89a5d66ff45dff8f2e4a19de96
Amanda Fernandes <aferanda@gmail.com>
2022-03-15 13:10:33 -0300

    feat: route delete user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const { createUser, getAllUsers, getUserById } = require('../controllers/user.controllers');
 const userMiddleware = require('../middlewares/user.middleware');
 const authMiddleware = require('../middlewares/auth.middleware');
+const {
+  createUser,
+  getAllUsers,
+  getUserById,
+  deleteUser,
+} = require('../controllers/user.controllers');
 
 const router = express.Router();
 
 router.post('/', userMiddleware, createUser);
 router.get('/', authMiddleware, getAllUsers);
 router.get('/:id', authMiddleware, getUserById);
+router.delete('/me', authMiddleware, deleteUser);
 
 module.exports = router;
