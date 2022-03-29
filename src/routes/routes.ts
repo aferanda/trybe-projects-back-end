56908c77b3039c58e1ef7af214baa693ccc0bd56
Amanda Fernandes <aferanda@gmail.com>
2022-03-28 23:19:00 -0300

    feat: define route to list products

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,3 +1,8 @@
 import { Router } from 'express';
+import productControllers from '../controllers/product.controllers';
+
 const routes = Router();
+
+routes.get('/products', productControllers.getAllProducts);
+
 export default routes;
