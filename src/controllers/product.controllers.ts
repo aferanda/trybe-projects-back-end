d0af7240f74a3bfd3f517782c1839e243b00c643
Amanda Fernandes <aferanda@gmail.com>
2022-03-29 02:01:25 -0300

    feat: function to create product

--- src/controllers/product.controllers.ts
+++ src/controllers/product.controllers.ts
@@ -6,6 +6,12 @@ class ProductController {
     const { code, products } = await productServices.getAllProducts();
     return res.status(code).json(products);
   };
+
+  createProduct = async (req: Request, res: Response) => {
+    const { code, item } = await productServices.createProduct(req.body);
+
+    return res.status(code).json({ item });
+  };
 }
 
 export default new ProductController();
