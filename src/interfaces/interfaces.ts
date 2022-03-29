565a1d6ab74942115c1836a7aec6a3c6d67a8a7e
Amanda Fernandes <aferanda@gmail.com>
2022-03-29 02:35:30 -0300

    feat: create token and user interfaces

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -2,3 +2,15 @@ export interface IProduct {
   name: string;
   amount: string;
 }
+
+export interface IToken {
+  id: number;
+  username: string;
+}
+
+export interface IUser {
+  username: string;
+  classe: string;
+  level: number;
+  password: string;
+}
