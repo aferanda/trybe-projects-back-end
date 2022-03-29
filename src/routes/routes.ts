b86ab91a8ae375c4642fa0fde5a2e817b292e01d
Amanda Fernandes <aferanda@gmail.com>
2022-03-29 10:49:26 -0300

    feat: update routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import middlewares from '../middlewares';
 import productControllers from '../controllers/product.controllers';
 import userControllers from '../controllers/user.controllers';
+import orderControllers from '../controllers/order.controllers';
 
 const routes = Router();
 
@@ -11,5 +12,7 @@ routes.get('/products', productControllers.getAllProducts);
 routes.post('/products', productMiddleware, productControllers.createProduct);
 
 routes.post('/users', userMiddleware, userControllers.createUser);
+
+routes.get('/orders', orderControllers.getAllOrders);
 
 export default routes;
