a61755cec45655253b05bd086eaa6db1b9fb0234
Amanda Fernandes <aferanda@gmail.com>
2022-03-29 18:01:16 -0300

    feat: create login middleware

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,9 +1,11 @@
 import userMiddleware from './user.middleware';
 import productMiddleware from './product.middleware';
 import errorMiddleware from './error.middleware';
+import loginMiddleware from './login.middleware';
 
 export default {
   userMiddleware,
   productMiddleware,
   errorMiddleware,
+  loginMiddleware,
 };
