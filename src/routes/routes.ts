3fbfcfb57b55b1da1c3009d7c3d717db7cec7a91
Amanda Fernandes <aferanda@gmail.com>
2022-03-29 02:02:19 -0300

    feat: define route to create product

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
+import productMiddleware from '../middlewares/product.middleware';
 import productControllers from '../controllers/product.controllers';
 
 const routes = Router();
 
 routes.get('/products', productControllers.getAllProducts);
+routes.post('/products', productMiddleware, productControllers.createProduct);
 
 export default routes;
